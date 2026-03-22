b94bb90f7c6d3d668415fee0e81c9e5b5cfa8780
ledhed2222 <ledhed2222@users.noreply.github.com>
2026-03-22 01:21:58 -0400

    eslinting and basic changes

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+]
