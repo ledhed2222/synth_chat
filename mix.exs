069663b76a9d94d07d91953dbcc32871de6bdfa2
ledhed2222 <ledhed2222@users.noreply.github.com>
2026-03-23 22:58:27 -0400

    don't use uuid package

--- mix.exs
+++ mix.exs
@@ -70,8 +70,7 @@ defmodule SupercolliderCubes.MixProject do
       ## additional dependencies
       {:membrane_core, "~> 1.2"},
       {:membrane_webrtc_plugin, "~> 0.26"},
-      {:membrane_opus_plugin, "~> 0.20"},
-      {:uuid, "~> 1.1"}
+      {:membrane_opus_plugin, "~> 0.20"}
     ]
   end
 
